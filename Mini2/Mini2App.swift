feb9021aa27f701bf1da370957f69d6655b7eed8
Vans <stevanstc2018@gmail.com>
2024-06-20 14:28:32 +0700

    Initial Commit

---
+++ Mini2/Mini2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini2App.swift
+//  Mini2
+//
+//  Created by Stevans Calvin Candra on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Mini2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
