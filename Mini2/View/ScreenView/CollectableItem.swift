57d049b6ed757ec5e72a24f472f55b99db4317d9
Putri Wulandari Oktaviani <putri.wulanoktv@gmail.com>
2024-06-24 17:29:43 +0700

    Time picker component and Set timer view

---
+++ Mini2/View/ScreenView/CollectableItem.swift
@@ -0,0 +1,18 @@
+//
+//  CollectableItem.swift
+//  Mini2
+//
+//  Created by Putri Wulandari Oktaviani on 24/06/24.
+//
+
+import SwiftUI
+
+struct CollectableItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CollectableItem()
+}
