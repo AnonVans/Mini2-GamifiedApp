3ee657f2631faacd329d8da4e31f8fc542f414ff
Jovanna Melissa <jovannamelissa6@gmail.com>
2024-06-25 18:50:28 +0700

    Make JSON data for the chickens and seperate view of before break

--- Mini2/View/ScreenView/BeforeBreakView.swift
+++ Mini2/View/ScreenView/BeforeBreakView.swift
@@ -10,33 +10,19 @@ import SwiftUI
 struct BeforeBreakView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     let eggTimer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
-    @State private var timeRemaining = 0.0
+    @State private var timeRemaining = 10.0
     @State private var format = "0:0"
-    @State private var yAmount = 100.0
+    @State private var yAmount = 200.0
     @State private var swipedUp = false
-    @State private var leftArrowXAmount = 60.0
-    @State private var rightArrowXAmount = 60.0
-    @State private var basketTapped = false
-    @State private var eggCounter = 0
-    @State private var basketPosition:CGPoint = CGPoint(x: 200.0, y: 670.0)
-    @State private var eggPositions: [CGPoint] = Array(repeating: CGPoint(x: 0, y: 200), count: 3)
-    @State private var eggDisappear: [Bool] = Array(repeating: false, count: 3)
-    @State private var currentEggIndex: Int = 0
-    @State private var chickenPositions: [CGPoint] = Array(repeating: CGPoint(x: 100, y: 100), count: 3)
     
     var body: some View {
-        NavigationStack{
+        VStack{
             ZStack{
                 Rectangle()
                     .foregroundStyle(Color.primary1)
                     .ignoresSafeArea()
                 
                 VStack{
-                    Text("Study Session 0 of 1")
-                        .font(.system(size: 17))
-                        .fontWeight(.semibold)
-                    
-                    if(timeRemaining > 0 && swipedUp == false){
                         Image("Ayam")
                             .padding(.top, 50)
                         
@@ -46,9 +32,9 @@ struct BeforeBreakView: View {
                                     timeRemaining -= 1
                                     
                                     if(yAmount >= 100.0){
-                                        yAmount -= 50
+                                        yAmount -= 150
                                     } else {
-                                        yAmount = 120.0
+                                        yAmount = 200.0
                                     }
                                 }
                             }
@@ -58,8 +44,8 @@ struct BeforeBreakView: View {
                             .onChange(of: timeRemaining){
                                 timeFormat(remainingTime:timeRemaining)
                             }
-                            .fontWeight(.regular)
-                            .font(.system(size: 56))
+                            .fontWeight(.bold)
+                            .font(.system(size: 48))
                             
                         Image("arrowupnew")
                             .position(CGPoint(x: 200.0, y: yAmount))
@@ -81,111 +67,9 @@ struct BeforeBreakView: View {
                             .fontWeight(.regular)
                             .font(.system(size: 27))
                             .padding(.bottom, 80)
-                    } else {
-                        
-                        if(basketTapped == false){
-                            Image("Ayam")
-                                .padding(.top, 50)
-                            
-                            HStack{
-                                Image("leftarrow")
-                                    .position(CGPoint(x: leftArrowXAmount, y: 400.0))
-                                    .animation(.easeInOut(duration: 1), value: leftArrowXAmount)
-                                
-                                
-                                Image("basket")
-                                    .position(CGPoint(x: 65.0, y: 400.0))
-                                    .onTapGesture {
-                                        basketTapped = true
-                                    }
-                                
-                                Image("rightarrow")
-                                    .position(CGPoint(x: rightArrowXAmount, y: 400.0))
-                                    .animation(.easeInOut(duration: 1), value: rightArrowXAmount)
-                            }
-                            .onReceive(timer) { input in
-                                if(basketTapped == false){
-                                    if(leftArrowXAmount >= 50 && rightArrowXAmount <= 80){
-                                        leftArrowXAmount -= 20
-                                        rightArrowXAmount += 20
-                                    } else {
-                                        leftArrowXAmount = 60
-                                        rightArrowXAmount = 60
-                                    }
-
-                                }
-                            }
-                        } else {
-                            ZStack{
-                                DraggableView(location: $basketPosition, imageName: "basket")
-                                    .onChange(of: basketPosition){newValue in
-                                        self.basketPosition.x = newValue.x
-                                        print("Basket position updated: \(newValue)")
-                                    }
-                                
-                                VStack{
-                                    ZStack {
-                                        ForEach(0..<eggPositions.count, id: \.self) { index in
-                                            Image("Ayam")
-                                                .padding(.top, 50)
-                                                .position(chickenPositions[index])
-                                                .opacity(index <= currentEggIndex && !eggDisappear[index] ? 1 : 0)
-                                            
-                                            Image("telor")
-                                                .resizable()
-                                                .frame(width: 60, height: 100)
-                                                .position(eggPositions[index])
-                                                .opacity(index <= currentEggIndex && !eggDisappear[index] ? 1 : 0)
-                                                .animation(.default, value: currentEggIndex)
-                                        }
-                                    }
-                                    .onAppear {
-                                        for i in 0..<eggPositions.count {
-                                            eggPositions[i].x = CGFloat.random(in: 50..<351)
-                                            chickenPositions[i].x = eggPositions[i].x
-                                        }
-                                    }
-                                    .onReceive(eggTimer) { _ in
-                                        if currentEggIndex < eggPositions.count {
-                                            withAnimation(Animation.easeInOut(duration: 1)) {
-                                                eggPositions[currentEggIndex].y = 650
-                                            } completion: {
-                                                checkEggCaught(currentEggIndex: currentEggIndex)
-                                                
-                                                currentEggIndex += 1
-                                            }
-                                            
-                                            
-                                            
-                                            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                                                                eggDisappear[currentEggIndex - 1] = true
-                                                            }
-                                        }
-                                    }
-                                }
-                                
-                                Spacer()
-                            }
-                        }
                     }
                 }
             }
-            
-        }
-        .navigationBarBackButtonHidden()
-    }
-    
-    private func checkEggCaught(currentEggIndex: Int) {
-        if(eggPositions[currentEggIndex].x <= basketPosition.x + 50 && eggPositions[currentEggIndex].x >= basketPosition.x - 50){
-            eggCounter += 1
-            print("yesss")
-            print("\(basketPosition.x) and \(eggPositions[currentEggIndex].x)")
-        } else {
-            print("no")
-            print("\(basketPosition.x) and \(eggPositions[currentEggIndex].x)")
-        }
-        
-        print("eggcounter: \(eggCounter)")
     }
 }
 
