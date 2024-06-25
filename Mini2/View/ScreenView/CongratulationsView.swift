b3b48ae85759a1fcd010e56d1292a3a678001482
venus <veshavesa@gmail.com>
2024-06-25 17:22:04 +0700

    Update CongratulationView

--- Mini2/View/ScreenView/CongratulationsView.swift
+++ Mini2/View/ScreenView/CongratulationsView.swift
@@ -9,9 +9,52 @@ import SwiftUI
 
 struct CongratulationsView: View {
     var body: some View {
-        VStack {
-            Text("Congratulations!")
-            Text("You have made it to the end")
+        ZStack {
+            VStack {
+                ZStack {
+                    
+                }
+                Image("congratsChicki")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .overlay(
+                        LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0), Color.white.opacity(10)]), startPoint: .top, endPoint: .bottom)
+                            .frame(height: 500)
+                            .padding(.top, 100)
+                    )
+                    .offset(y: 50)
+                
+                Text("Congratulations!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.bottom)
+                    .offset(y: -100)
+                    .foregroundColor(.blue)
+                
+                Text("You have made it to the end")
+                    .font(.body)
+                    .fontWeight(.bold)
+                    .offset(y: -120)
+                    .foregroundColor(.blue)
+                
+                Spacer()
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Back to Timer")
+                        .fontWeight(.semibold)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 3, x: 3, y: 3)
+                }
+            }
+            
+            
+            
+            
         }
         
     }
